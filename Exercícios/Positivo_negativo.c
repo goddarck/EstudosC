4b3a6746e2dac57eed50fb92fde28a231e79ace6
goddarck <102770877+goddarck@users.noreply.github.com>
2022-03-31 20:30:23 -0300

    First commit

--- Exercícios/Positivo_negativo.c
+++ Exercícios/Positivo_negativo.c
@@ -1,1 +1,23 @@
+	#include <stdio.h>
+	#include <stdlib.h>
 
+
+	int main(){
+		int numero;
+		printf("\nDigite o numero: ");
+		scanf("%i" , &numero);
+
+	if ((numero%2 == 0)&&(numero > 0))
+		printf("O numero %i e Positivo e Par!\n",numero);	
+		if ((numero%2 != 0)&&(numero > 0))
+			printf("O numero %i e Positivo e Impar!\n",numero);
+
+	if ((numero%2 == 0)&&(numero < 0))
+		printf("O numero %i e Negativo e Par!\n",numero);	
+		if ((numero%2 != 0)&&(numero < 0))
+			printf("O numero %i e Negativo e Impar!\n",numero);
+
+	if (numero == 0)
+			{printf("O numero %i e Neutro!\n", 0);}
+
+}
